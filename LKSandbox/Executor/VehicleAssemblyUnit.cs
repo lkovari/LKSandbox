da495e561fe1adbb3dd8c153a285a22fb19b5126
LK <laszlo.kovary@gmail.com>
2023-04-30 19:49:54 +0200

    removed IVehicleAssemblyUnit

--- LKSandbox/Executor/VehicleAssemblyUnit.cs
+++ LKSandbox/Executor/VehicleAssemblyUnit.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace LKSandbox.Executor
 {
-    internal class VehicleAssemblyUnit<T> where T : IVehicleParts //,IVehicleAssemblyUnit<T>
+    internal class VehicleAssemblyUnit<T> where T : IVehicleParts
     {
         public IVehicleEngineData VehicleBuilder(T item)
         {

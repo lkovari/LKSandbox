1e96f0186a53505e904975e6de426f7ad04426db
LK <laszlo.kovary@gmail.com>
2023-05-21 22:33:41 +0200

    TwinsDetector tests

--- LKSandboxTest/TwinsTest.cs
+++ LKSandboxTest/TwinsTest.cs
@@ -7,28 +7,40 @@ namespace LKSandboxTest
 {
     public class TwinsTest
     {
-        List<ICreated> creates;
+        List<ICreated> createsTest1;
+        List<ICreated> createsTest2;
         [SetUp]
         public void Setup()
         {
-            creates = new List<ICreated>();
-            creates.Add(new Created(new DateOnly(1965, 2, 2)));
-            creates.Add(new Created(new DateOnly(1965, 2, 3)));
-            creates.Add(new Created(new DateOnly(1966, 2, 2)));
-            creates.Add(new Created(new DateOnly(1966, 12, 10)));
-            creates.Add(new Created(new DateOnly(1966, 12, 11)));
-            creates.Add(new Created(new DateOnly(2023, 4, 3)));
-            creates.Add(new Created(new DateOnly(2023, 4, 5)));
-            creates.Add(new Created(new DateOnly(1923, 5, 21)));
-            creates.Add(new Created(new DateOnly(1923, 5, 22)));
-            creates.Add(new Created(new DateOnly(2023, 5, 21)));
-            creates.Add(new Created(new DateOnly(2023, 5, 21)));
+            createsTest1 = new List<ICreated>();
+            createsTest1.Add(new Created(new DateOnly(1965, 2, 2)));
+            createsTest1.Add(new Created(new DateOnly(1965, 2, 3)));
+            createsTest1.Add(new Created(new DateOnly(1966, 2, 2)));
+            createsTest1.Add(new Created(new DateOnly(1966, 12, 10)));
+            createsTest1.Add(new Created(new DateOnly(1966, 12, 11)));
+            createsTest1.Add(new Created(new DateOnly(2023, 4, 3)));
+            createsTest1.Add(new Created(new DateOnly(2023, 4, 5)));
+            createsTest1.Add(new Created(new DateOnly(1923, 5, 21)));
+            createsTest1.Add(new Created(new DateOnly(1923, 5, 22)));
+            createsTest1.Add(new Created(new DateOnly(2023, 5, 21)));
+            createsTest1.Add(new Created(new DateOnly(2023, 5, 21)));
+            createsTest2 = new List<ICreated>();
+            createsTest2.Add(new Created(new DateOnly(1965, 2, 2)));
+            createsTest2.Add(new Created(new DateOnly(1966, 2, 2)));
+            createsTest2.Add(new Created(new DateOnly(1966, 12, 11)));
+            createsTest2.Add(new Created(new DateOnly(2023, 4, 3)));
+            createsTest2.Add(new Created(new DateOnly(2023, 4, 5)));
+            createsTest2.Add(new Created(new DateOnly(1923, 5, 21)));
+            createsTest2.Add(new Created(new DateOnly(1923, 5, 23)));
+            createsTest2.Add(new Created(new DateOnly(2023, 5, 21)));
+            createsTest2.Add(new Created(new DateOnly(2023, 5, 23)));
+
         }
         [Test]
-        public void TwinsDetectionTest()
+        public void TwinsDetectionTest1()
         {
-            IEnumerable<IEnumerable<ICreated>> twins = TwinsDetector.Detect(creates);
-            twins.Should().HaveCount(4);
+            IEnumerable<IEnumerable<ICreated>> twins = TwinsDetector.Detect(createsTest1);
+            twins.Should().HaveCountGreaterThan(0);
             foreach (var twin in twins)
             {
                 twin.Should().NotBeNull();
@@ -39,6 +51,11 @@ namespace LKSandboxTest
                 diff.Should().BeLessThan(2);
             }
         }
-
+        [Test]
+        public void TwinsDetectionTest2()
+        {
+            IEnumerable<IEnumerable<ICreated>> twins = TwinsDetector.Detect(createsTest2);
+            twins.Should().HaveCountLessThan(1);
+        }
     }
 }
